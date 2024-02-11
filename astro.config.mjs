c5daac6fe184bcf04f0d623299355b4f9e3755cd
Sergiy Seletskyy <sseletskyy@gmail.com>
2024-02-11 19:15:55 +0200

    configure deploy (take 2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sseletskyy.github.io',
-    base: '/portfolio',
+    base: '/',
     output: 'static',
     integrations: [
         mdx(),
